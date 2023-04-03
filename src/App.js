f72da324c7442b10d0dac415367a02fdf39ba4b6
Mohammed Thalha <mohammed.t@ibridgellc.com>
2023-04-03 17:27:32 +0530

    part 37 render props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import ClickCounter from './components/ClickCounter';
+import Counter from './components/Counter';
 import HoverCounter from './components/HoverCounter';
 
 const App = () => {
   return (
     <div className="App">
-      <ClickCounter />
-      <HoverCounter />
+      <Counter
+        render={(counter, incrementCount) => (
+          <ClickCounter counter={counter} incrementCount={incrementCount} />
+        )}
+      />
+
+      <Counter
+        render={(counter, incrementCount) => (
+          <HoverCounter counter={counter} incrementCount={incrementCount} />
+        )}
+      />
     </div>
   );
 }
